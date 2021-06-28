37377eb0f2a83d2de625342dfec81ca5096cd932
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-06-28 13:10:08 +0200

    Conseguimos la estetica principal, pero tenemos que esperar a una buena sesion de fotos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule , Routes } from '@angular/router';
+import { MainComponent } from './componentes/main/main.component';
+import { NavegacionComponent } from './componentes/navegacion/navegacion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:MainComponent},
+  {path:'**',redirectTo:'',pathMatch:'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavegacionComponent
   ],
   imports: [
     BrowserModule,
