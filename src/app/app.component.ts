825e7895a0e661affb2f37c6a780c4c9a8e3c536
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-07-01 17:02:28 +0200

    Hacemos commit antes de implantar una cosilla

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FondoeventoService } from './servicios/fondoevento.service';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
   title = 'portfolio2';
+  wallpapa!:string;
+  
+  mapwp(resp:[number,string]){
+    let caso:string = "url(../assets/img/fotos/";
+    if(resp[0] >= 5){
+      switch(resp[1]){
+        case "/" : caso += this._fe.imgurl.about.pic ; break ;
+        case "/perfil" : caso += this._fe.imgurl.perfil.pic ; break ;
+        case "/trabajos" : caso += this._fe.imgurl.trabajos.pic ; break ;
+        case "/rrss" : caso += this._fe.imgurl.rrss.pic ; break ;
+      }
+    } else {
+      switch(resp[1]){
+        case "/" : caso += this._fe.imgurl.about.subpic ; break ;
+        case "/perfil" : caso += this._fe.imgurl.perfil.subpic ; break ;
+        case "/trabajos" : caso += this._fe.imgurl.trabajos.subpic ; break ;
+        case "/rrss" : caso += this._fe.imgurl.rrss.subpic ; break ;
+      }
+    }
+    caso += ".jpg);";
+    return caso;
+  }
+
+  constructor( private _fe:FondoeventoService){
+    this._fe.superobs$.pipe(
+      tap(resp => {
+        this.wallpapa = this.mapwp(resp);
+        console.log(this.mapwp(resp));
+      })
+    ).subscribe();
+  }
 }
+
+/*
+export class AppComponent {
+  title = 'portfolio2';
+  wallpapa!:string;
+  constructor( private _fe:FondoeventoService ){
+    const restest = (resp:[number,string]) => {
+      let caso!:string;
+      if(resp[0] >= 5){
+        caso = "url(../assets/img/fototest.jpg)"
+      } else {
+        caso = "url(../assets/img/fototestsujeto.jpg)"
+      }
+      return caso;
+    }
+    this._fe.superobs$.pipe(
+      tap(resp => this.wallpapa = restest(resp))
+    ).subscribe()
+  }
+}
+*/
--- src/app/servicios/fondoevento.service.ts
+++ src/app/servicios/fondoevento.service.ts
@@ -7,51 +7,45 @@ import { distinctUntilChanged , tap , map, combineAll } from 'rxjs/operators';
 })
 export class FondoeventoService {
 
-  //Observable de responsividad:
-  subres$ = new BehaviorSubject<number>(document.documentElement.clientWidth);
-  obsres$ = this.subres$.pipe(
-    map(resp => this.resnumero(resp)),
-    distinctUntilChanged()
-  );
-  //Observable de fondo:
-  subwall$ = new BehaviorSubject<string>(window.location.pathname);
-
-  //Conjunto:
-  superobs$ = combineLatest([this.obsres$,this.subwall$]);
-
-  resnumero(arg:number){
-    let x:number = arg; let caso!:number;
-    switch(true){
-      case x >= 1120 : caso = 5 ; break ;
-      case x <= 1120 && x >= 801 : caso = 4 ; break ;
-      case x <= 800 && x >= 661 : caso = 3 ; break ;
-      case x <= 660 && x >= 451 : caso = 2 ; break ;
-      case x <= 450 : caso = 1 ; break ;
-    }
-    return caso;
+  //Objeto de direcciones de fondos:
+  imgurl = {
+    about : { pic: "dragon" , subpic:"dragonsujeto"},
+    perfil : { pic: "lobo" , subpic:"lobosujeto"},
+    trabajos : { pic: "oso" , subpic:"ososujeto"},
+    rrss : { pic: "tighe" , subpic:"tighesujeto"},
   }
+  
+  //Observables y mecanismos:
+
+    //Observable de responsividad:
+    subres$ = new BehaviorSubject<number>(document.documentElement.clientWidth);
+    obsres$ = this.subres$.pipe(
+      map(resp => this.resnumero(resp)),
+      distinctUntilChanged()
+    );
+    //Observable de fondo:
+    subwall$ = new BehaviorSubject<string>(window.location.pathname);
+    //Conjunto:
+    superobs$:Observable<[number,string]> = combineLatest([this.obsres$,this.subwall$]);
+    //Funcion de resolucion
+    resnumero(arg:number){
+      let x:number = arg; let caso!:number;
+      switch(true){
+        case x >= 1120 : caso = 5 ; break ;
+        case x <= 1120 && x >= 801 : caso = 4 ; break ;
+        case x <= 800 && x >= 661 : caso = 3 ; break ;
+        case x <= 660 && x >= 451 : caso = 2 ; break ;
+        case x <= 450 : caso = 1 ; break ;
+      }
+      return caso;
+    }
+  
+  
   
   constructor(){
     const prueba = () => {this.subres$.next(document.documentElement.clientWidth);}
     window.addEventListener("resize",prueba);
     this.superobs$.subscribe(console.log);
   }
-
-  /*
-  test$ = new Subject<number>();
-  valor:number = 15;
-  
-  constructor(){
-    this.test$.subscribe(console.log);
-  }
-
-  ngOnInit(): void {
-  }
-
-  testeando(){
-    console.log("Entra en la funcion")
-    this.test$.next(20);
-  }
-  */
 
 }
