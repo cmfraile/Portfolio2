101d549e2a689bec419adac84a9b2730f63e10d4
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-07-05 13:06:33 +0200

    Vamos a implantar Renderer2, pero debido a dudas lo vamos a probar en sandbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { FondoeventoService } from './servicios/fondoevento.service';
 import { map } from 'rxjs/operators';
 
@@ -8,12 +8,13 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./app.component.sass']
 })
 
-export class AppComponent{
+export class AppComponent {
   title = 'portfolio2';
-
   
+  @ViewChild('mipadre',{ static : false }) mipadrelet !: ElementRef;
+
   mapwp(resp:[number,string]){
-    let caso:string = "url('../assets/img/fotos/";
+    let caso:string = "'url(../assets/img/fotos/";
     if(resp[0] >= 5){
       switch(resp[1]){
         case "/" : caso += this._fe.imgurl.about.pic ; break ;
@@ -29,24 +30,24 @@ export class AppComponent{
         case "/rrss" : caso += this._fe.imgurl.rrss.subpic ; break ;
       }
     }
-    caso += ".jpg');";
+    caso += ".jpg)';";
     return caso
   }
 
-  constructor( private _fe:FondoeventoService ){
+  constructor( private _fe:FondoeventoService , private _r2:Renderer2){
     this._fe.superobs$.pipe(
       map(resp => this.mapwp(resp)),
     ).subscribe(resp => {
-      //this.padre.nativeElement. = resp
-
+      this._r2.setStyle(this.mipadrelet.nativeElement,"background-image",resp);
     });
   }
 
   /*
-  constructor( private _fe:FondoeventoService){
+  constructor( private _fe:FondoeventoService , private _r:Renderer2){
     this._fe.superobs$.pipe(
       map(resp => this.mapwp(resp)),
-    ).subscribe(console.log);
+    ).subscribe();
   }
   */
+
 }
