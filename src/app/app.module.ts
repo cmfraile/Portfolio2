868bf933c4ada40cc52ec3b0f77e0878de336755
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-09-16 14:58:23 +0200

    implantado MATERIAL. El tema de la fuente de letra lo veremos para despues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { PerfilComponent } from './componentes/perfil/perfil.component';
 import { TrabajosComponent } from './componentes/trabajos/trabajos.component';
 import { RrssComponent } from './componentes/rrss/rrss.component';
 import { PaneladminModule, routes as rutaspaneladmin } from './paneladmin/paneladmin.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const routes: Routes = [
   {path:'',component:MainComponent},
@@ -33,7 +34,8 @@ const routes: Routes = [
     PaneladminModule,
     RouterModule.forRoot(routes,{
       scrollPositionRestoration:'top',
-    })
+    }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/paneladmin/paneladmin.module.ts
+++ src/app/paneladmin/paneladmin.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { MainComponent } from './componentes/main/main.component';
 import { Routes,RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSliderModule } from '@angular/material/slider';
 
 export const routes : Routes = [
   { path:'',component:MainComponent }
@@ -14,7 +15,8 @@ export const routes : Routes = [
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule
+    RouterModule,
+    MatSliderModule
   ]
 })
 export class PaneladminModule { }
