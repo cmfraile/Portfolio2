fdc4efd4b72356c26d2a4ee26a67f12368f5185c
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-10-26 13:34:27 +0200

    checkpoint previo a implantar un pipe para el periodo de formacion

--- src/app/componentes/perfil/perfil.component.ts
+++ src/app/componentes/perfil/perfil.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { dinteres, experiencia, formacion } from 'src/app/interfaces/todainterfaz';
 import { FondoService } from 'src/app/servicios/fondo.service';
+import { TraerdataService } from 'src/app/servicios/traerdata.service';
 
 @Component({
   selector: 'app-perfil',
@@ -9,7 +11,19 @@ import { FondoService } from 'src/app/servicios/fondo.service';
 })
 export class PerfilComponent implements OnInit{
 
-  constructor(private _r:Router , private _fs:FondoService){}
+  experiencia!:experiencia[];
+  formacion!:formacion[];
+  dinteres!:dinteres[];
+  
+  constructor(private _td:TraerdataService){
+    this._td.submaster.experiencia$.subscribe(resp => this.experiencia = resp);
+    this._td.submaster.formacion$.subscribe(resp => this.formacion = resp);
+    this._td.submaster.dinteres$.subscribe(resp => this.dinteres = resp);
+  }
+
+  periodopipe(dato:any):string{
+    if(dato.length == 1){return `${dato[0]}aaa`}else{return `${dato[0]} - ${dato[1]}aaa`};
+  }
 
   ngOnInit(): void {}
 
