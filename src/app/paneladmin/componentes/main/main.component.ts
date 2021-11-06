bbc2d93b78bca673c081eb2a3d09681edffc53b0
CarlosFraile <carlosfrailegomez93@gmail.com>
2021-11-06 10:44:20 +0100

    implantamos una funcion para deshabilitar el panel de administracion con las ventanas pequeñas

--- src/app/paneladmin/componentes/main/main.component.ts
+++ src/app/paneladmin/componentes/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { tap } from 'rxjs/operators';
 import { HeartbeatService } from '../../servicios/heartbeat.service';
 import { MenucambioService } from '../../servicios/menucambio.service';
 
@@ -11,10 +13,17 @@ export class MainComponent implements OnInit {
 
   desplieguemenu!:string;
   flagacceso!:boolean;
+  anchomovil!:boolean;
   
   constructor( private _mc:MenucambioService , private _hb:HeartbeatService ){
     this._mc.menuadmin$.subscribe(resp => this.desplieguemenu = resp);
     this.flagacceso = this._hb.latido();
+    fromEvent(window,'resize').pipe(
+      tap(resp => {
+        let ancho:number = window.innerWidth;
+        if(ancho <= 1000){this.anchomovil = false}else{this.anchomovil = true};
+      })
+    ).subscribe(resp => {console.log(this.anchomovil)});
   }
 
   ngOnInit(): void {
