b5140d2905fb80ca9a4db8a7549562b80100ffdb
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-04-24 17:15:42 +0200

    carpinteria del infierno realisada

--- src/app/componentes/perfil/perfil.component.ts
+++ src/app/componentes/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 import { dinteres, experiencia, formacion } from 'src/app/interfaces/todainterfaz';
 import { FondoService } from 'src/app/servicios/fondo.service';
 import { TraerdataService } from 'src/app/servicios/traerdata.service';
@@ -15,7 +16,16 @@ export class PerfilComponent implements OnInit{
   dinteres!:dinteres[]|undefined;
   
   constructor(private _td:TraerdataService , public _f:FondoService){
-    this._td.perfilGET.experiencia$.subscribe(resp => this.experiencia = resp);
+    this._td.perfilGET.experiencia$.pipe(
+      map<experiencia[],experiencia[]>( (resp) => {
+        resp.sort( (a,b) => {
+          const reobj = { a:(a.descripcion.split('')[0] == "_") ? true : false , b:(b.descripcion.split('')[0] == "_") ? true : false };
+          if(reobj.a && !reobj.b){return 1} ; if(!reobj.a && reobj.b){return -1} ; return 0 ;
+        });
+        resp.map((x) => {if(x.descripcion.substring(0,1) == "_"){x.descripcion = x.descripcion.substring(1)}})
+        return resp
+      })
+    ).subscribe(resp => this.experiencia = resp);
     this._td.perfilGET.formacion$.subscribe(resp => this.formacion = resp);
     this._td.perfilGET.dinteres$.subscribe(resp => this.dinteres = resp);
   }
