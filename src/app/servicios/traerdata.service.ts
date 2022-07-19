9ccb50832b80754c0c3bc0ef835438fe617e8b3d
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-07-19 17:34:03 +0200

    correcsion

--- src/app/servicios/traerdata.service.ts
+++ src/app/servicios/traerdata.service.ts
@@ -19,20 +19,20 @@ export class TraerdataService {
   ntairGET = this._hc.get<di.ntair[]>(`${this.baseURL}/ntair`).pipe(map(resp => resp[0]));
   trabajosGET = this._hc.get<di.trabajo[]>(`${this.baseURL}/trabajo`).pipe(
     map(x => {
-      //Fecha vs no fecha
-      x = x.sort((a,b) => {
-        const regex = new RegExp('[0-9][0-9] - [0-9][0-9][0-9][0-9]');
-        const aregex = (regex.test(a.estado)) ? 1 : 0 ; const bregex = (regex.test(b.estado)) ? 1 : 0 ;
-        return bregex - aregex ;
+      const regex = new RegExp('[0-9][0-9] - [0-9][0-9][0-9][0-9]') ; let nofechas = x.filter(x => !regex.test(x.estado)) ; let fechas = x.filter(x => regex.test(x.estado));
+      fechas.map(x => {x.fecha = {year:parseInt(x.estado.substring(x.estado.length - 4,x.estado.length)),month:parseInt(x.estado.substring(0,2))}});
+      fechas.sort((a,b) => {
+        let amonth = a.fecha?.month || 0 ; let bmonth = b.fecha?.month || 0 ;
+        if(amonth > bmonth){return -1} ; if(bmonth > amonth){return 1};
+        return 0;
+      })
+      fechas.sort((a,b) => {
+        let ayear = a.fecha?.year || 0 ; let byear = b.fecha?.year || 0 ;
+        if(ayear > byear){return -1} ; if(byear > ayear){return 1};
+        return 0;
       });
-      x = x.sort((a,b) => {
-        const ayear = parseInt(a.estado.substring(a.estado.length - 4,a.estado.length)) ; const byear = parseInt(a.estado.substring(a.estado.length - 4,a.estado.length));
-        if(ayear == byear){return 0} ; if(!ayear){return 1} ; if(!byear){return -1} ;
-        if(ayear > byear){return 1} ; if(byear < ayear){return -1} ; return 0;
-      });
-      return x;
+      return new Array().concat(fechas,nofechas);
     }),
-    tap(console.log)
   );
   perfilGET = {
     experiencia$ : this._hc.get<di.experiencia[]>(`${this.baseURL}/experiencia`),
