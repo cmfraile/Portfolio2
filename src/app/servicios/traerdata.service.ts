1d281f6614daa41ea136f88bcb64d7430015a595
Carlos Manuel Fraile Gomez <carlosfrailegomez93@gmail.com>
2022-07-19 15:44:02 +0200

    achcuhe

--- src/app/servicios/traerdata.service.ts
+++ src/app/servicios/traerdata.service.ts
@@ -17,7 +17,23 @@ export class TraerdataService {
   
   headermaster = new HttpHeaders({token:sessionStorage.getItem('token') || ""});
   ntairGET = this._hc.get<di.ntair[]>(`${this.baseURL}/ntair`).pipe(map(resp => resp[0]));
-  trabajosGET = this._hc.get<di.trabajo[]>(`${this.baseURL}/trabajo`);
+  trabajosGET = this._hc.get<di.trabajo[]>(`${this.baseURL}/trabajo`).pipe(
+    map(x => {
+      //Fecha vs no fecha
+      x = x.sort((a,b) => {
+        const regex = new RegExp('[0-9][0-9] - [0-9][0-9][0-9][0-9]');
+        const aregex = (regex.test(a.estado)) ? 1 : 0 ; const bregex = (regex.test(b.estado)) ? 1 : 0 ;
+        return bregex - aregex ;
+      });
+      x = x.sort((a,b) => {
+        const ayear = parseInt(a.estado.substring(a.estado.length - 4,a.estado.length)) ; const byear = parseInt(a.estado.substring(a.estado.length - 4,a.estado.length));
+        if(ayear == byear){return 0} ; if(!ayear){return 1} ; if(!byear){return -1} ;
+        if(ayear > byear){return 1} ; if(byear < ayear){return -1} ; return 0;
+      });
+      return x;
+    }),
+    tap(console.log)
+  );
   perfilGET = {
     experiencia$ : this._hc.get<di.experiencia[]>(`${this.baseURL}/experiencia`),
     formacion$ : this._hc.get<di.formacion[]>(`${this.baseURL}/formacion`),
